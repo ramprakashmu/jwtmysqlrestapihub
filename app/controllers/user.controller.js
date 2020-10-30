03042d246b5f6fdc6323c64a235eb782dd50a989
Ramprakash Munugapati <ramprakashmu@yahoo.com>
2020-10-30 22:40:45 +0530

    Initial Commit
    Initial Commit

---
+++ app/controllers/user.controller.js
@@ -0,0 +1,19 @@
+exports.allAccess = (req, res) => {
+  res.status(200).send("Public Content.");
+};
+
+exports.studentBoard = (req, res) => {
+  res.status(200).send("Student Content.");
+};
+
+exports.adminBoard = (req, res) => {
+  res.status(200).send("Admin Content.");
+};
+
+exports.teacherBoard = (req, res) => {
+  res.status(200).send("Teacher Content.");
+};
+
+exports.parentBoard = (req, res) => {
+  res.status(200).send("Parent Content.");
+};
